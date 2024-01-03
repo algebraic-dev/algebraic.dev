4e195f6ee77916f2322802d6bed66930287482ae
Sofia Rodrigues <algebraic.sofia+github@gmail.com>
2024-01-03 01:08:31 -0300

    feat: finish background

--- src/lib/Background.svelte
+++ src/lib/Background.svelte
@@ -1,50 +1,79 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
+    type Line = {
+        x: number,
+        y: number,
+        angle: number
+    }
+
 	let canvas: HTMLCanvasElement;
 	let context: CanvasRenderingContext2D | null;
 
 	let innerWidth: number = 0;
 	let innerHeight: number = 0;
     let interval: number = 57;
-    let displacementX: number = 20;
-    let displacementY: number = 20;
-    let mouseDisplacement: number = 10;
+    let displacementX: number = 1;
+    let displacementY: number = 1;
+    let mouseDisplacementX: number = 10;
+    let angleTilt = 0.1;
 
-    let angle = 0;
+    let angle = Math.PI/180 * 30;
     
+    let setup: boolean = false;
+    let initialX = 0;
+    let initialY = 0;
+
     let mouseX = 0;
     let mouseY = 0;
 
     let cameraX = 0;
     let cameraY = 0;
 
+    let intersectY = (a: Line, x: number) => Math.tan(a.angle) * (x - a.x) + a.y;
+    let intersectX = (a: Line, y: number) => (y - a.y) / Math.tan(a.angle) + a.x;
+
     const lerp = (x: number, y: number, a: number) => x * (1 - a) + y * a;
 
     const handleMove = (event: MouseEvent) => {
+
+        if (!setup) {
+            initialX = event.clientX;
+            initialY = event.clientY;
+            setup = true;
+        }
+
         mouseX = event.clientX;
         mouseY = event.clientY;
     }
 
-    const drawLine = (orientation: 'vertical' | 'horizontal', position: number) => {
+    const drawLine = (line: Line) => {
         if (context) {
             
+            let intersectionTop = intersectY(line, 0);
+            let intersectionDown = intersectY(line, innerWidth);
+
+            let intersectLeft = intersectX(line, 0);
+            let intersectRight = intersectX(line, innerHeight);
+
             context.beginPath();
-            context.strokeStyle = 'rgba(157, 157, 157, 0.20)';
-            context.lineWidth = 1;
+                context.strokeStyle = `rgba(157, 157, 157, 0.2)`;
+                context.lineWidth = 1;
+                
+                if (intersectionTop < 0) {
+                    context.moveTo(intersectLeft, 0);
+                } else {
+                    context.moveTo(0, intersectionTop);
+                }
 
-            displacementX = Math.sin(Date.now() / 3000) * 20 + 10;
-            displacementY = Math.sin(Date.now() / 5670) * 17 + 7;
+                if (intersectionDown > innerHeight) {
+                    context.lineTo(intersectRight, innerHeight);
+                } else {
+                    context.lineTo(innerWidth, intersectionDown);
+                }
+        
+                context.stroke();
 
-            if (orientation === 'vertical') {
-                context.moveTo(position, 0);
-                context.lineTo(position, innerHeight);
-            } else {
-                context.moveTo(0, position);
-                context.lineTo(innerWidth, position);
-            }
-
-            context.stroke();
         }
     }
 
@@ -52,17 +81,30 @@
 		context = canvas.getContext('2d');
 
         const update = () => {
-            cameraX = lerp(cameraX, mouseX, 0.05);
-            cameraY = lerp(cameraY, mouseY, 0.05);
+            cameraX = lerp(cameraX, mouseX - initialX, 0.05);
+            cameraY = lerp(cameraY, mouseY - initialY, 0.05);
+
+            angle = Math.sin(performance.now() / 1000) * Math.PI/180 * angleTilt + 30;
+
+            interval = Math.sin(performance.now() / 1000) * 0.1 + 70;
+
 
             context?.clearRect(0, 0, innerWidth, innerHeight);
 
-            for (let i = 0; i < innerWidth / interval; i++) {
-                drawLine('vertical', i * interval - displacementX + cameraX/mouseDisplacement);
-            }
+            let widthIntervals = innerWidth / interval;
+            let heightInterval = innerHeight / interval;
 
-            for (let i = 0; i < innerHeight / interval; i++) {
-                drawLine('horizontal', i * interval - displacementY + cameraY/mouseDisplacement);
+            for (let i = -widthIntervals; i < widthIntervals*2; i++) {
+                let x = (i * interval)*Math.cos(angle) + innerWidth/2 + cameraX/mouseDisplacementX + displacementX;
+                let y = (i * interval)*Math.sin(angle) + innerHeight/2 + cameraY/mouseDisplacementX + displacementY;
+                drawLine({x, y, angle: angle + Math.PI/2 })
+            }
+            
+            for (let i = -heightInterval; i < heightInterval*2; i++) {
+                let nAngle = angle + Math.PI/2;
+                let x = (i * interval)*Math.cos(nAngle) + innerWidth/2 + cameraX/mouseDisplacementX + displacementX;
+                let y = (i * interval)*Math.sin(nAngle) + innerHeight/2 + cameraY/mouseDisplacementX + displacementY;
+                drawLine({x, y, angle: nAngle + Math.PI/2 })
             }
             
             requestAnimationFrame(update);
