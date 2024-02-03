62ed0adf9a3aebc8c4cd62ac16fa091f5281d2ae
Sofia Rodrigues <algebraic.sofia+github@gmail.com>
2024-02-03 16:48:26 -0300

    chore: remove useless file

--- a.rs
+++
@@ -1,8 +0,0 @@
-macro_rules! stack_operations {
-    ($type:ty, $prefix:ident) => {
-      pub fn $(stringify!($prefix, 2b))(&mut self) {
-        let inner: $type = self.pop().into();
-        self.push(MistValue::Byte(inner as i8));
-      }
-    };
-  }
